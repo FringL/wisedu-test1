526c81f14e829aa8159ce45763dd163cc21ba539
FringLin <fringlin@126.com>
2020-04-09 01:45:25 +0800

    修整代码

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $.ajax({
     dataType: "json", //返回数据格式为json
     success: function(data) {//请求成功完成后要执行的方法 
         if (data.code == 1 && data.msg == "成功") {
-            console.log(data.msg);  
             data.result.forEach(article => {
                 //文章标题
                 var articleTitle = article.title;
@@ -14,23 +13,30 @@ $.ajax({
                 var nowTime = new Date();
                 var monthDifference = nowTime.getMonth() - writeTime.getUTCMonth();
                 var articleTime = null;
+                var dayDifference = nowTime.getDay() - writeTime.getDay();
+                var hourDifference = nowTime.getHours() - writeTime.getHours();
+                var minuteDifference = nowTime.getMinutes() - writeTime.getMinutes();
+
                 if (monthDifference < 1){
-                    var dayDifference = nowTime.getDay() - writeTime.getDay();
                     if (dayDifference < 1){
-                        var hourDifference = nowTime.getHours() - writeTime.getHours();
-                        if(hourDifference < 1){
-                            var minuteDifference = nowTime.getMinutes() - writeTime.g
+                        if (hourDifference < 1){
+                            if (minuteDifference <1){
+                                articleTime = "刚刚发布";
+                            } else{
+                                articleTime = minuteDifference - 1 + "分钟前发布";
+                            }
+                        } else{
+                            articleTime = hourDifference - 1 + "小时前发布";
                         }
                     }
                     else{
-                        articleTime = dayDifference + "天前发布";
+                        articleTime = dayDifference - 1 + "天前发布";
                     }
                 } else if (monthDifference <= 12){
                     articleTime = monthDifference - 1 + "个月前发布";
                 } else{
                     articleTime = "一年以前发布";
                 }
-                //console.log(articleTime);
 
                 //计算阅读人数(同一篇文章已读，阅读数加一)
                 var readTimes = 0;
@@ -40,11 +46,11 @@ $.ajax({
                     }
                 });
                 var articleRead = readTimes + "人已阅读";
-                console.log(articleRead);
 
                 //图片url
                 var articleImage = article.img;
 
+                //添加li标签
                 document.getElementById("articles").innerHTML+="<div class=\"split-line\"></div><li class=\"article\"><br><div class=\"article-content\"><span class=\"article-time\">" + articleTime +  "</span><br><span class=\"article-read\">" + articleRead + "</span><br><img class=\"article-image\" src=\"" + articleImage + "\"></div><br><a class=\"article-title\" href=\"\">" + articleTitle +"</a><br></li>";
             });
         }   
