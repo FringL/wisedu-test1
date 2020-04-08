2dd5f729285fe83ce1154a0c623b64c624458118
FringLin <fringlin@126.com>
2020-04-09 01:35:12 +0800

    通过读取JSON数据实现动态页面

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-// window.onload = function () {
-//     var url = "data.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-//     var request = new XMLHttpRequest();
-//     request.open("get", url);/*设置请求方法与路径*/
-//     request.send(null);/*不发送数据到服务器*/
-//     request.onload = function () {/*XHR对象获取到返回信息后执行*/
-//         if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
-//             var json = JSON.parse(request.responseText);
-//             console.log(json.msg);
-//         }
-//     }
-// }
+$.ajax({
+    url: "data.json",//json文件位置
+    type: "GET",//请求方式为get
+    dataType: "json", //返回数据格式为json
+    success: function(data) {//请求成功完成后要执行的方法 
+        if (data.code == 1 && data.msg == "成功") {
+            console.log(data.msg);  
+            data.result.forEach(article => {
+                //文章标题
+                var articleTitle = article.title;
+
+                //计算发布时间
+                var writeTime = new Date(article.time);
+                var nowTime = new Date();
+                var monthDifference = nowTime.getMonth() - writeTime.getUTCMonth();
+                var articleTime = null;
+                if (monthDifference < 1){
+                    var dayDifference = nowTime.getDay() - writeTime.getDay();
+                    if (dayDifference < 1){
+                        var hourDifference = nowTime.getHours() - writeTime.getHours();
+                        if(hourDifference < 1){
+                            var minuteDifference = nowTime.getMinutes() - writeTime.g
+                        }
+                    }
+                    else{
+                        articleTime = dayDifference + "天前发布";
+                    }
+                } else if (monthDifference <= 12){
+                    articleTime = monthDifference - 1 + "个月前发布";
+                } else{
+                    articleTime = "一年以前发布";
+                }
+                //console.log(articleTime);
+
+                //计算阅读人数(同一篇文章已读，阅读数加一)
+                var readTimes = 0;
+                data.result.forEach(article => {
+                   if(article.status == "1" ){
+                       readTimes++;
+                    }
+                });
+                var articleRead = readTimes + "人已阅读";
+                console.log(articleRead);
+
+                //图片url
+                var articleImage = article.img;
+
+                document.getElementById("articles").innerHTML+="<div class=\"split-line\"></div><li class=\"article\"><br><div class=\"article-content\"><span class=\"article-time\">" + articleTime +  "</span><br><span class=\"article-read\">" + articleRead + "</span><br><img class=\"article-image\" src=\"" + articleImage + "\"></div><br><a class=\"article-title\" href=\"\">" + articleTitle +"</a><br></li>";
+            });
+        }   
+    }
+ })
